040768cde0eac024b2892ba664a96cc4fcf1a5c8
Shital8596 <101247710+Shital8596@users.noreply.github.com>
2022-08-28 00:25:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import './styles/App.css';
-import './components/Heading'
+import 'src/components/Heading.js'
 import './components/InputQuery'
 import './components/SubHeading'
 import './components/SubmitButton'
