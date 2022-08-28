e78aded8cf8ff23e1ae5bef8f56cae6a8fda092f
Shital8596 <101247710+Shital8596@users.noreply.github.com>
2022-08-29 00:12:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component, useState } from "react";
+import ReactDOM from "react-dom";
 import './styles/App.css';
 import './Heading.js';
 import './InputQuery.js';
@@ -6,7 +7,7 @@ import './SubHeading.js';
 import './SubmitButton.js';
 
 const App = () => {
-  return ReactDOM.render(
+  return (
     <div id="main">
       <Heading />
       <InputQuery />
@@ -15,8 +16,7 @@ const App = () => {
     </div>
   )
 }
-<div id="app"></div> 
-ReactDOM.render(<App />, document.querySelector("#app"));
+ReactDOM.render(App(), document.getElementById("root1"));
 
 export default App
 
